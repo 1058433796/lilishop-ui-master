83c3e74f6da5d620165268a734ec9e62620da773
chuanranchuanran <34791646+chuanranchuanran@users.noreply.github.com>
2022-09-27 20:34:11 +0800

    修改菜单栏

--- seller/src/libs/routerJson.js
+++ seller/src/libs/routerJson.js
@@ -83,129 +83,8 @@ export const result = [{
             component: "order/order/orderList",
             children: null,
           },
-          // {
-          //   name: "virtualOrderList",
-          //   level: 2,
-          //   type: 0,
-          //   keepAlive: true,
-          //   title: "虚拟订单",
-          //   path: "virtualOrderList",
-          //   component: "order/order/virtualOrderList",
-          //   children: null
-          // }
         ]
       },
-      // {
-      //   name: "memberComment",
-      //   level: 1,
-      //   type: 0,
-      //   title: "评价管理",
-      //   path: "/order",
-      //   component: "Main",
-      //   children: [{
-      //     name: "memberComment",
-      //     level: 2,
-      //     type: 0,
-      //     title: "评价管理",
-      //     path: "memberComment",
-      //     component: "member/memberComment",
-      //     children: null,
-      //   }]
-      // },
-      // {
-      //   name: "order",
-      //   level: 1,
-      //   type: 0,
-      //   title: "售后管理",
-      //   path: "/order",
-      //   component: "Main",
-      //   children: [{
-      //       name: "returnGoodsOrder",
-      //       level: 2,
-      //       type: 0,
-      //       keepAlive: true,
-      //       title: "退货管理",
-      //       path: "returnGoodsOrder",
-      //       component: "order/after-order/returnGoodsOrder",
-      //       children: null
-      //     },
-      //     {
-      //       name: "returnMoneyOrder",
-      //       level: 2,
-      //       type: 0,
-      //       keepAlive: true,
-      //       title: "退款管理",
-      //       path: "returnMoneyOrder",
-      //       component: "order/after-order/returnMoneyOrder",
-      //       children: null
-      //     },
-      //     {
-      //       name: "orderComplaint",
-      //       level: 2,
-      //       type: 0,
-      //       keepAlive: true,
-      //       title: "投诉管理",
-      //       path: "orderComplaint",
-      //       component: "order/after-order/orderComplaint",
-      //       children: null
-      //     }
-      //   ]
-      // }
-    ]
-  },
-  {
-    name: "bill",
-    level: 0,
-    type: -1,
-    title: "财务",
-    path: "",
-    component: "",
-    children: [{
-        name: "bill",
-        level: 1,
-        type: 0,
-        title: "财务管理",
-        path: "/bill",
-        component: "Main",
-        children: [{
-            name: "accountStatementBill",
-            level: 2,
-            type: 0,
-            keepAlive: true,
-            title: "财务详情",
-            path: "accountStatementBill",
-            component: "shop/bill/accountStatementBill",
-            children: null
-          },
-          // {
-          //   name: "storeBill",
-          //   level: 2,
-          //   type: 0,
-          //   keepAlive: true,
-          //   title: "店铺结算",
-          //   path: "storeBill",
-          //   component: "shop/bill/storeBill",
-          //   children: null
-          // }
-        ]
-      },
-      // {
-      //   name: "receiptManager",
-      //   level: 1,
-      //   type: 0,
-      //   title: "发票管理",
-      //   path: "/bill",
-      //   component: "Main",
-      //   children: [{
-      //     name: "receipt",
-      //     level: 2,
-      //     type: 0,
-      //     title: "发票管理",
-      //     path: "receipt",
-      //     component: "order/receiptManager/receipt",
-      //     children: null
-      //   }]
-      // }
     ]
   },
   {
@@ -232,35 +111,9 @@ export const result = [{
             component: "shop/bill/accountStatementBill",
             children: null
           },
-          // {
-          //   name: "storeBill",
-          //   level: 2,
-          //   type: 0,
-          //   keepAlive: true,
-          //   title: "店铺结算",
-          //   path: "storeBill",
-          //   component: "shop/bill/storeBill",
-          //   children: null
-          // }
         ]
       },
-      // {
-      //   name: "receiptManager",
-      //   level: 1,
-      //   type: 0,
-      //   title: "发票管理",
-      //   path: "/bill",
-      //   component: "Main",
-      //   children: [{
-      //     name: "receipt",
-      //     level: 2,
-      //     type: 0,
-      //     title: "发票管理",
-      //     path: "receipt",
-      //     component: "order/receiptManager/receipt",
-      //     children: null
-      //   }]
-      // }
+     
     ]
   },
   {
@@ -287,99 +140,8 @@ export const result = [{
             component: "promotion/pintuan/pintuan",
             children: null
           },
-          // {
-          //   name: "seckill",
-          //   level: 2,
-          //   type: 0,
-          //   keepAlive: true,
-          //   title: "秒杀活动",
-          //   path: "seckill",
-          //   component: "promotion/seckill/seckill",
-          //   children: null
-          // }
         ]
       },
-      // {
-      //   name: "lives",
-      //   level: 1,
-      //   type: 0,
-      //   title: "直播活动",
-      //   path: "/promotion",
-      //   component: "Main",
-      //   children: [{
-      //       name: "live",
-      //       level: 2,
-      //       type: 0,
-      //       title: "直播管理",
-      //       path: "live",
-      //       component: "promotion/live/live",
-      //       children: null
-      //     },
-      //     {
-      //       name: "liveGoods",
-      //       level: 2,
-      //       type: 0,
-      //       title: "直播商品",
-      //       path: "liveGoods",
-      //       component: "promotion/live/liveGoods",
-      //       children: null
-      //     }
-      //   ]
-      // },
-      // {
-      //   name: "storePromotion",
-      //   level: 1,
-      //   type: 0,
-      //   title: "商家活动",
-      //   path: "/storePromotion",
-      //   component: "Main",
-      //   children: [{
-      //       name: "full-cut",
-      //       level: 2,
-      //       type: 0,
-      //       title: "满额活动",
-      //       path: "full-discount",
-      //       component: "promotion/full-discount/full-discount",
-      //       children: null
-      //     },
-      //     {
-      //       name: "coupon",
-      //       level: 2,
-      //       type: 0,
-      //       title: "优惠券",
-      //       path: "coupon",
-      //       component: "promotion/coupon/coupon",
-      //       children: null
-      //     },
-      //   ]
-      // },
-      // {
-      //   name: "distribution",
-      //   level: 1,
-      //   type: 0,
-      //   title: "分销管理",
-      //   path: "/distribution",
-      //   component: "Main",
-      //   children: [{
-      //       name: "distributionGoods",
-      //       level: 2,
-      //       type: 0,
-      //       title: "分销商品",
-      //       path: "distributionGoods",
-      //       component: "distribution/distributionGoods",
-      //       children: null
-      //     },
-      //     {
-      //       name: "distributionOrder",
-      //       level: 2,
-      //       type: 0,
-      //       title: "分销订单",
-      //       path: "distributionOrder",
-      //       component: "distribution/distributionOrder",
-      //       children: null
-      //     }
-      //   ]
-      // }
     ]
   },
   {
@@ -405,24 +167,7 @@ export const result = [{
           component: "statistics/goods",
           children: null
         },
-        // {
-        //   name: "orderStatistics",
-        //   level: 2,
-        //   type: 0,
-        //   title: "订单统计",
-        //   path: "orderStatistics",
-        //   component: "statistics/order",
-        //   children: null
-        // },
-        // {
-        //   name: "trafficStatistics",
-        //   level: 2,
-        //   type: 0,
-        //   title: "流量统计",
-        //   path: "trafficStatistics",
-        //   component: "statistics/traffic",
-        //   children: null
-        // },
+       
       ]
     }]
   },
@@ -435,42 +180,7 @@ export const result = [{
     component: "",
     description: "",
     children: [
-      // {
-      //   name: "user-manage",
-      //   level: 1,
-      //   type: 0,
-      //   title: "页面设置",
-      //   path: "user-manage",
-      //   component: "sys/user-manage/userManage",
-      //   children: []
-      // },
-      // {
-      //   name: "ship",
-      //   level: 1,
-      //   type: 0,
-      //   title: "配送设置",
-      //   path: "",
-      //   component: "Main",
-      //   children: [{
-      //       name: "shipTemplate",
-      //       level: 2,
-      //       type: 0,
-      //       title: "配送模板",
-      //       path: "shipTemplate",
-      //       component: "shop/ship/shipTemplate",
-      //       children: null
-      //     },
-      //     {
-      //       name: "logistics",
-      //       level: 2,
-      //       type: 0,
-      //       title: "物流公司",
-      //       path: "logistics",
-      //       component: "shop/ship/logistics",
-      //       children: null
-      //     }
-      //   ]
-      // },
+    
       {
         name: "shop",
         level: 1,
@@ -488,15 +198,6 @@ export const result = [{
             component: "shop/shopSetting",
             children: null,
           },
-          // {
-          //   name: "shopAddress",
-          //   level: 2,
-          //   type: 0,
-          //   title: "自提管理",
-          //   path: "shopAddress",
-          //   component: "shop/shopAddress",
-          //   children: null,
-          // }
         ]
       }
     ]
