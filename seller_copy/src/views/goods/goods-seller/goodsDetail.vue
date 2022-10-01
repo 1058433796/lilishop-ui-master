55e0565813913f8c6d96e83aeca421b9185b8691
1058433796 <1058433796@qq.com>
2022-10-01 16:19:17 +0800

    添加goodsDetail页面并在router注册

---
+++ seller_copy/src/views/goods/goods-seller/goodsDetail.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <p>产品详情</p>
+  <Divider/>
+  <Row>
+    <Col span="12">
+        <div class="div">
+            <span>名称：</span>
+            <span>合页</span>
+        </div>  
+        
+        <div class="div">
+            <span>性能规格：</span>
+            <span>合页</span>
+        </div>  
+
+        <div class="div">
+            <span>材质：</span>
+            <span>合页</span>
+        </div>  
+
+        <div class="div">
+            <span>饰面：</span>
+            <span>合页</span>
+        </div>  
+
+        <div class="div">
+            <span>品牌：</span>
+            <span>合页</span>
+        </div>  
+
+        <div class="div">
+            <span>价格：</span>
+            <span>合页</span>
+        </div>  
+        <div class="div">
+            <span>联系人：</span>
+            <span>合页</span>
+        </div>  
+
+        <div class="div">
+            <span>联系电话：</span>
+            <span>合页</span>
+        </div>  
+        <div class="div">
+            <span>邮箱：</span>
+            <span>合页</span>
+        </div>  
+        <div class="div">
+            <span>检测报告：</span>
+            <span>合页</span>
+        </div>  
+        <div class="div">
+            <span>说明书：</span>
+            <span>合页</span>
+        </div>  
+    </Col>
+    <Col span="12">
+        图片Col
+        <Button type="primary">导出PDF</Button>
+    </Col>
+  </Row>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
